18dca4f2ac5766fd7db2d170efd919721c29b4a0
o-louis <oriane.louis.pro@gmail.com>
2021-03-04 23:53:53 -0500

    ✨ Hooks - useScroll

--- hooks/useScroll.js
+++ hooks/useScroll.js
@@ -1,7 +1,23 @@
 import { useEffect, useState } from "react";
 
-const useScroll = () => {
-  return;
+const useScroll = (element) => {
+  const [pos, setPos] = useState({x: 0, y: 0})
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      setPos({
+        x: e.target.scrollLeft || window.pageXOffset || 0,
+        y: e.target.scrollTop || window.pageYOffset || 0,
+      })
+    }
+
+    element = element?.current || window;
+    element.addEventListener('scroll', handleScroll);
+
+    return () => element.removeEventListener('scroll', handleScroll);
+  }, [element])
+
+  return pos;
 }
 
-export default useScroll
+export default useScroll;
