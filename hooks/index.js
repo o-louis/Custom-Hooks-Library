a2e55e1cd1a51a07b4a5f594762b6cad689a85ea
o-louis <oriane.louis.pro@gmail.com>
2021-03-10 20:29:20 -0500

    ✨ Create index for the list of the hooks

---
+++ hooks/index.js
@@ -0,0 +1,5 @@
+export { default as useFetch } from "./useFetch";
+export { default as useKeyDown } from "./useKeyDown";
+export { default as useLocalStorage } from "./useLocalStorage";
+export { default as useScroll } from "./useScroll";
+export { default as useTheme } from "./useTheme";
