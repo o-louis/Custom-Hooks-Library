97aba80e0ce456c1cc06601302fd5510c9e68227
Oriane <31882053+o-louis@users.noreply.github.com>
2021-03-12 21:35:07 -0500

    ✨ Update index.js with useToggler export

--- hooks/index.js
+++ hooks/index.js
@@ -3,3 +3,4 @@ export { default as useKeyDown } from "./useKeyDown";
 export { default as useLocalStorage } from "./useLocalStorage";
 export { default as useScroll } from "./useScroll";
 export { default as useTheme } from "./useTheme";
+export { default as useToggler } from "./useToggler";
